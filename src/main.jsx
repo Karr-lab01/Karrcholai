fea73fc9272bd064aa6760e2455282d65b16361a
Harish-dev01 <harishm.dev01@gmail.com>
2026-08-10 22:52:10 +0530

    up

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,17 @@ import App from './App.jsx'
 import './index.css'
 import './styles/global.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
     <HelmetProvider>
       <App />
     </HelmetProvider>
   </React.StrictMode>,
 )
+
+// After React mounts, hide the SEO shell via CSS class (not JS display:none)
+// Crawlers that don't run JS will always see the full SEO shell content.
+requestAnimationFrame(() => {
+  document.documentElement.classList.add('react-ready')
+})
